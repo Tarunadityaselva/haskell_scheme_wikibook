abc5cedb3bb3431c3a2b45c5f006db93baf0432b
tarunadityaselva <tarunadityaselva@gmail.com>
2020-08-11 20:46:49 +0530

    completed writing a simple parser before the exercise portion and Recursive Parsers

---
+++ simple_parser.hs
@@ -0,0 +1,57 @@
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
+
+symbol::Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+--parseExpr :: Parser LispVal
+--parseExpr = parseAtom
+--         <|> parseString
+--         <|> parseNumber
+
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many (noneOf "\"")
+                char '"'
+                return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+              first <- letter <|> symbol
+              rest <- many (letter <|> digit <|> symbol)
+              let atom = first:rest 
+              return $ case atom of 
+                        "#t" -> Bool True
+                        "#f" -> Bool False
+                        _    -> Atom atom 
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+                
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of 
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
+
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+
+main :: IO()
+main = do 
+          (expr:_) <- getArgs
+          putStrLn (readExpr expr)
